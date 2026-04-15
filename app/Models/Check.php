6e6f6487b5d7b23ae9b158d4f2356826b066a012
John Dave Leongson <daveleongson28@gmail.com>
2026-04-15 13:04:53 +0800

    "Update: Create for New Check Button"

--- app/Models/Check.php
+++ app/Models/Check.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Check extends Model
 {
-    //
+    use HasFactory;
+
+    // Eto yung idadagdag natin para payagan ma-save itong mga fields na 'to mula sa form
+    protected $fillable = [
+        'check_number',
+        'voucher_number',
+        'voucher_date',
+        'payee',
+        'amount',
+        'remarks',
+        'account_description',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,11 @@ Route::get('dashboard', function () {
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
     Route::get('/profile-pictures', [ProfilePictureController::class, 'index'])->name('profile-pictures.index');
+    
+    // Checks Routes
     Route::get('/checks', [CheckController::class, 'index'])->name('checks.index');
+    Route::get('/checks/create', [CheckController::class, 'create'])->name('checks.create'); // Eto ang magpapakita ng form
+    Route::post('/checks', [CheckController::class, 'store'])->name('checks.store');         // Eto ang magse-save ng data
     Route::get('/checks/{check}/print', [CheckController::class, 'show'])->name('checks.print');
 });
 
